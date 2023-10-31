94aaf4c12f7babc8ed6394ed7016f695e4e8e2d5
Santanu Sen <santanusen82@gmail.com>
2023-10-31 08:18:14 +0530

    DC 231031

--- 02433_arr_from_prefix_xor.cc
+++ 02433_arr_from_prefix_xor.cc
@@ -1,6 +1,12 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
+        for (int i = 1, prv = pref[0]; i < pref.size(); pref[i] ^= prv, ++i)
+            swap(pref[i], prv);
+        return pref;
+    }
+
+    vector<int> findArray2(vector<int>& pref) {
         for (int i = pref.size() - 1; i > 0; --i) 
             pref[i] ^= pref[i - 1];
         return pref;
